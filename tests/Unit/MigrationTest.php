54a73f1680d54ee65e9ecca0b769fae92ea480a7
Mohamed Allam <meda3gm@gmail.com>
2021-05-07 14:15:17 +0100

    refactoring migration test file

--- tests/Unit/MigrationTest.php
+++ tests/Unit/MigrationTest.php
@@ -4,9 +4,7 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Artisan;
 
 class MigrationTest extends TestCase
 {
